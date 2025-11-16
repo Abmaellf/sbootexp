5994e8ed27cee5807862282f0b27bb82a10d687b
Abmaellf <abmael.ninha@gmail.com>
2025-11-16 14:19:34 -0400

    first commit

---
+++ src/main/java/com/abmael/sbootexp/SbootexpApplication.java
@@ -0,0 +1,13 @@
+package com.abmael.sbootexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abmael/sbootexp/SbootexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abmael.sbootexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
