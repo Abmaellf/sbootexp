cd8f9232ceed838b5eedae7c8d931d49770e7f90
Abmaellf <abmael.ninha@gmail.com>
2025-11-16 16:04:22 -0400

    feat: entendendo o Authentication

--- src/main/java/com/abmael/sbootexp/api/fooController.java
+++ src/main/java/com/abmael/sbootexp/api/fooController.java
@@ -1,6 +1,7 @@
 package com.abmael.sbootexp.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class fooController {
 
     @GetMapping("/public")
-    public ResponseEntity<String> publicRoutr() {
-        return ResponseEntity.ok("Public route ok");
+    public ResponseEntity<String> publicRoutr(Authentication authentication) {
+        System.out.println(authentication.getClass());
+        return ResponseEntity.ok("Public route ok "+authentication.getName());
     }
 
     @GetMapping("/private")
-    public ResponseEntity<String> privateRoutr() {
-        return ResponseEntity.ok("Private route ok");
+    public ResponseEntity<String> privateRoutr(Authentication authentication) {
+        System.out.println(authentication.getClass());
+        return ResponseEntity.ok("Private  route  ok "+authentication.getName() );
     }
 
 
