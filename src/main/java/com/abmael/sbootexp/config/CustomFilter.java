bba65773df8081666337a37bad0f0743f91f3b4d
Abmaellf <abmael.ninha@gmail.com>
2025-11-18 23:40:57 -0400

    feat: add filter chain  custon

---
+++ src/main/java/com/abmael/sbootexp/config/CustomFilter.java
@@ -0,0 +1,39 @@
+package com.abmael.sbootexp.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+// Executa todas as vezes uma vez durate a requisição, intercept todas as requisições
+//Uma das formas do usuário se autenticar é no cabeçalho
+@Component
+public class CustomFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+                    HttpServletRequest request,
+                    HttpServletResponse response,
+                    FilterChain filterChain) throws ServletException, IOException {
+        String secretHeader = request.getHeader("x-secret");
+        if(secretHeader != null) {
+           if( secretHeader.equals("secr3t")) {
+               Authentication authentication =  new UsernamePasswordAuthenticationToken(
+                       "Muito secreto", null,
+                       List.of(new SimpleGrantedAuthority("ADMIN")));
+               SecurityContext securityContex = SecurityContextHolder.getContext();
+               securityContex.setAuthentication(authentication);
+           }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/abmael/sbootexp/config/SecurityConfig.java
+++ src/main/java/com/abmael/sbootexp/config/SecurityConfig.java
@@ -12,13 +12,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider,
+            CustomFilter customFilter) throws  Exception {
         return http
                 .authorizeHttpRequests(cutomizer -> {
                     cutomizer.requestMatchers("/public").permitAll();
@@ -26,6 +30,8 @@ public class SecurityConfig {
                 })
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
+                .authenticationProvider(senhaMasterAuthenticationProvider) //Sobrescreve o authenticationProvider do spring security que é o UserDetail
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
